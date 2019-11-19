15064e5a6a6667cc9e2d96c84d261403ed57de0d
HC2MAC19 <hc2mac19@iMac-10.local>
2019-11-18 21:57:12 -0300

    Adicionados alguns arquivos de configuração(Não finalizados)

---
+++ trabalho-final-ios/ApiControllers/FilmApiController.swift
@@ -0,0 +1,12 @@
+//
+//  FilmApiController.swift
+//  trabalho-final-ios
+//
+//  Created by HC2MAC19 on 18/11/2019.
+//  Copyright © 2019 Thiago Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class FilmApiController {
+}
---
+++ trabalho-final-ios/Config/apiConfig.swift
@@ -0,0 +1,13 @@
+//
+//  apiConfig.swift
+//  trabalho-final-ios
+//
+//  Created by HC2MAC19 on 18/11/2019.
+//  Copyright © 2019 Thiago Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class apiConfig {
+    static let apiKey = "6302949d7afd94f9edd948caf36b33c2"
+}
